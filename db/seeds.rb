eac8f40f4be54d87c076fc237e36927fd91c3774
Sebastian Cerda <sebastian.cerda@ssindex.org>
2024-03-29 16:12:37 -0300

    feat(db): ADD status and last_updated_at values on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Restaurant.create(name: 'Niu Sushi MidMall Maipu')
-Restaurant.create(name: 'Niu Sushi Pajaritos - Arauco Maipu')
+Restaurant.create(name: 'Niu Sushi MidMall Maipu', status: 'OK', last_updated_at: Time.current)
+Restaurant.create(name: 'Niu Sushi Pajaritos - Arauco Maipu', status: 'Error', last_updated_at: Time.current)
 p "Created #{Restaurant.count} Restaurant"
