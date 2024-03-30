05cde52c96e3562deffb1d716f586d1300739c0d
Sebastian Cerda <sebastian.cerda@ssindex.org>
2024-03-30 11:07:05 -0300

    feat(db): ADD devices data on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
 Restaurant.create(name: 'Niu Sushi MidMall Maipu', status: 'OK', last_updated_at: Time.current)
 Restaurant.create(name: 'Niu Sushi Pajaritos - Arauco Maipu', status: 'Error', last_updated_at: Time.current)
-p "Created #{Restaurant.count} Restaurant"
+
+Restaurant.all.each do |restaurant|
+  Device.create!([
+    'Impresora Epson 3423',
+    'Servidor Web',
+    'Servidor base de datos'
+  ].map{ |name|
+    {
+      name: name,
+      status: 'OK',
+      last_updated_at: Time.current,
+      is_central: false,
+      restaurant_id: restaurant.id
+    }
+  })
+end
+
+p "Created #{Restaurant.count} Restaurant with devices"
