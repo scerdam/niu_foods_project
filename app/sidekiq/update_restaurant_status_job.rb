2e12bdcc6063ff67f78bc0e9cf057ac380c79ab4
Sebastian Cerda <sebastian.cerda@ssindex.org>
2024-03-30 11:54:55 -0300

    feat(sidekiq): ADD Job for update restaurants status based on devices status

---
+++ app/sidekiq/update_restaurant_status_job.rb
@@ -0,0 +1,24 @@
+class UpdateRestaurantStatusJob
+  include Sidekiq::Job
+
+  def perform
+    restaurants = Restaurant.all
+    restaurants.each do |restaurant|
+      restaurant_status = calculate_restaurant_status(restaurant)
+      restaurant.update(status: restaurant_status, last_updated_at: Time.now)
+    end
+    p 'Restaurants status updated!'
+  end
+
+  private
+
+  def calculate_restaurant_status(restaurant)
+    if restaurant.devices.all? { |device| device.status == 'error' }
+      'error'
+    elsif restaurant.devices.all? { |device| device.status == 'ok' }
+      'ok'
+    else
+      'warning'
+    end
+  end
+end
