28de5dc280c2a5a1507cad1e1db057bfea836c99
Sebastian Cerda <sebastian.cerda@ssindex.org>
2024-03-30 13:00:43 -0300

    feat(helpers>application): ADD methods for format date and badge color by status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def niufoods_datetime_format(date)
+    date.strftime('%H:%M:%S - %d/%m/%y')
+  end
+
+  def generate_status_badge(status)
+    "<span class='badge rounded-pill bg-#{color_pill(status)}'>#{status}</span>".html_safe
+  end
+
+  def color_pill(status)
+    return 'success' if status == 'ok'
+
+    return 'danger' if status == 'error'
+
+    'warning'
+  end
 end
