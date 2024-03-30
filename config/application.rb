5477bc4961f6aced8313708588bcf726d1ab4221
Sebastian Cerda <sebastian.cerda@ssindex.org>
2024-03-30 11:22:10 -0300

    feat(config): ADD missing sidekiq configutation

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module NiuFoodsProject
     #
     config.time_zone = 'Santiago'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq
   end
 end
